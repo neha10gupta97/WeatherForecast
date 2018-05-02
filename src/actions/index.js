7a81432619180bddf01cb92eba8e7e964d51053f
Neha Gupta <neha10gupta97@gmail.com>
2018-05-03 00:01:41 +0530

    ajax call to fetch weather data for city by axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = "992ad088de029a224cff088df46f0258";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},ind`;
+  const request = axios.get(url);
+
+  return{
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}

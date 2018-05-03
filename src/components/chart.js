4ec8ec0113ff2288187b8172c4ccc06c28247450
Neha Gupta <neha10gupta97@gmail.com>
2018-05-03 21:36:29 +0530

    temp,humidity,pressure

--- src/components/chart.js
+++ src/components/chart.js
@@ -3,13 +3,14 @@
 //=> functional component
 
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 export default (props) => {
   return(
     <div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color}/>
+        <SparklinesReferenceLine type="avg"/>
       </Sparklines>
     </div>
   );
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -6,13 +6,15 @@ class WeatherList extends Component {
   renderWeather(cityData){
     const name = cityData.city.name;
     const temps =cityData.list.map(weather => weather.main.temp);
+    const humidities =cityData.list.map(weather => weather.main.humidity);
+    const pressures =cityData.list.map(weather => weather.main.pressure);
 
     return(
       <tr key={name}>
         <td>{name}</td>
-        <td>
-          <Chart color="orange" data={temps}/>
-        </td>
+        <td><Chart color="red" data={temps}/></td>
+        <td><Chart color="blue" data={pressures}/></td>
+        <td><Chart color="green" data={humidities}/></td>
       </tr>
     );
   }

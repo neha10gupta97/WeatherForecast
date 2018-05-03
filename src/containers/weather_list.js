719b3d26f35efa5b20c5f54db76b918d40c607ef
Neha Gupta <neha10gupta97@gmail.com>
2018-05-03 17:45:28 +0530

    weather list for city started

---
+++ src/containers/weather_list.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class WeatherList extends Component {
+  renderWeather(cityData){
+    const name = cityData.city.name;
+    const temps =cityData.list.map(weather => weather.main.temp);
+    console.log(temps);
+
+    return(
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+
+  render(){
+    return(
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Teamperatre</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    )
+  }
+}
+
+function mapStateToProps({weather}){
+  return {weather};
+}
+
+// function mapStateToProps(state){
+//   return {weather: state.weather };
+// }
+//we can write this same fn as :
+//function mapStateToProps({weather}[is eq to state.weather]){
+//   return {weather: weather} => further shrink to => return {weather}
+// }
+
+export default connect(mapStateToProps)(WeatherList);

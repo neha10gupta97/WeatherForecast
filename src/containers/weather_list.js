396caac84697aa237be3b25f20aa0ec6e270b713
Neha Gupta <neha10gupta97@gmail.com>
2018-07-07 00:13:58 +0530

    weather forecast complete

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,20 +1,25 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
+
   renderWeather(cityData){
     const name = cityData.city.name;
-    const temps =cityData.list.map(weather => weather.main.temp);
+    // const temps = cityData.list.map(weather => weather.main.temp);
     //convert temp in C
-    // const temps =_.map(cityData.list.map(weather => weather.main.temp),temp=>temp-273);
-    const humidities =cityData.list.map(weather => weather.main.humidity);
-    const pressures =cityData.list.map(weather => weather.main.pressure);
+    const temps =_.map(cityData.list.map(weather => weather.main.temp),temp=>temp-273);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+
+    const { lon, lat } = cityData.city.coord;
 
     return(
       <tr key={name}>
-        <td>{name}</td>
-        <td><Chart color="red" data={temps} units="K"/></td>
+        <td>{name}<GoogleMap long={lon} lat={lat}/></td>
+        <td><Chart color="red" data={temps} units="C"/></td>
         <td><Chart color="blue" data={pressures} units="hPa"/></td>
         <td><Chart color="green" data={humidities} units="%"/></td>
       </tr>
